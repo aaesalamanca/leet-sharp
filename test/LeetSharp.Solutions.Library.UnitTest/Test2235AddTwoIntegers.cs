c1fba310c6417ba7b3661b4197ffd68d24db583a
aaesalamanca <aaesalamanca@gmail.com>
2025-03-17 17:29:18 +0100

    refactor: Import static members from static classes into tests

--- test/LeetSharp.Solutions.Library.UnitTest/Test2235AddTwoIntegers.cs
+++ test/LeetSharp.Solutions.Library.UnitTest/Test2235AddTwoIntegers.cs
@@ -1,10 +1,12 @@
 namespace LeetSharp.Solutions.Library.UnitTest;
 
+using static Problem2235AddTwoIntegers;
+
 public class Test2235AddTwoIntegers
 {
     [Theory]
     [InlineData(12, 5, 17)]
     [InlineData(-10, 4, -6)]
     public void AddTwoIntegers_ReturnsTarget(int num1, int num2, int expected) =>
-        Assert.Equal(expected, Problem2235AddTwoIntegers.Sum(num1, num2));
+        Assert.Equal(expected, Sum(num1, num2));
 }
